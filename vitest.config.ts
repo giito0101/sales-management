7d6f20d8d5b9774388766f69a4cf0b4f3b5d6f71
giito <giito0101@gmail.com>
2026-02-04 23:04:59 +0900

    test: vitestの対象を限定

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import path from "node:path";
 
 export default defineConfig({
+  root: __dirname,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
@@ -11,5 +12,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
+    include: ["**/*.test.ts", "**/*.test.tsx"],
+    exclude: ["tests/**", "**/node_modules/**"],
   },
 });
